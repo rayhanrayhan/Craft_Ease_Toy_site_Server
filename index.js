be15150cbc3a3ee11b055c3618a982108761e29d
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-05-23 22:06:11 +0600

    get my toys done

--- index.js
+++ index.js
@@ -49,6 +49,15 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/myToys', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = toysCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
