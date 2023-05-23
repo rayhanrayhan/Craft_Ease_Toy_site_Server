6542896f1611ac701f8c5a523e6ea02316d4e5e4
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-05-23 21:12:23 +0600

    toy add done

--- index.js
+++ index.js
@@ -36,14 +36,18 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/allToys', async (req, res) => {
+            const newToy = req.body;
+            const result = await toysCollection.insertOne(newToy);
+            res.send(result)
+        })
+
         app.get('/viewDetails/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await toysCollection.findOne(query)
             res.send(result)
         })
-
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
